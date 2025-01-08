689a9b5fb47a29f30c1076e22d572d66b6487e8f
peros <g00422221@atu.ie>
2025-01-08 19:48:03 +0000

    created payment controller

---
+++ src/main/java/ie/atu/PaymentController.java
@@ -0,0 +1,15 @@
+package ie.atu;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/payment")
+@RestController
+public class PaymentController {
+
+    @PostMapping
+    private String makePayment(@RequestBody Person person) {
+        return "Got person details" + person;
+    }
+}
